9cbd9a66f501ba0ccf2afc68119379d951801fd3
harry <dammykuye@gmail.com>
2022-07-06 22:52:22 +0100

    configure image upload

--- storefront/store/signals.py
+++ storefront/store/signals.py
@@ -3,6 +3,9 @@ from .models import Customer
 from django.dispatch import receiver
 from django.conf import settings
 
+
+
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_customer_for_user(sender, **kwargs):
     """ automatically creates a customer once a user is registered """
--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -145,3 +145,6 @@ DJOSER = {
         'user_create': 'core.serializers.UserCreateSerializer',
         'current_user':'core.serializers.CurrentUserSerializer'}
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
