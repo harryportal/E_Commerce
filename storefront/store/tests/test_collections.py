43d27d941322181b92a34a296e79f4f67aa6ef97
harry <dammykuye@gmail.com>
2022-07-08 11:39:34 +0100

    started test

--- storefront/store/tests/test_collections.py
+++ storefront/store/tests/test_collections.py
@@ -1,8 +1,34 @@
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
+
+@pytest.mark.django_db
 class TestCreateCollection:
+
     def test_anonymous_user_401(self):
         client = APIClient()
-        response = client.post('/store/collections/', {'title':'a'})
+        response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_user_not_Admin_403(self):
+        client = APIClient()
+        user = client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_admin_invalid_data(self):
+        client = APIClient()
+        user = client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_admin_valid_data_401(self):
+        client = APIClient()
+        user = client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/',{'title':'a'})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
